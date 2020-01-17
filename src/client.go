9af55b07df72ece203d5551308a035d1c341252e
Ruslan <ruslan.rusu@yahoo.com>
2020-01-16 22:51:10 -0500

    count size

--- src/client.go
+++ src/client.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 func main() {
-	
+	var messagesCount = flag.Int("messages", 100, "messages to send over")
+	var messageSize = flag.Int("size", 100, "message size in bytes")
+
+	flag.Parse()
+
+	fmt.Print("workload of ", *messagesCount ," messages of size ", *messageSize)
+
+	for i := 0; i < *messagesCount; i++ {
+		fmt.Println("send ", i)
+	}
 }
